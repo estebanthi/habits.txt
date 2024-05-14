dbd47e0f0ad75af3f4d29c13bcc0d57c3c54912c
estebanthi <esteban.thilliez@gmail.com>
2024-05-14 21:44:25 +0200

    config command in CLI

--- habits_txt/cli.py
+++ habits_txt/cli.py
@@ -1,11 +1,10 @@
-import argparse
 import datetime as dt
-import logging
-
-import habits_txt.defaults as defaults
-import habits_txt.journal as journal
 
 import click
+
+import habits_txt.config as config_
+import habits_txt.defaults as defaults
+import habits_txt.journal as journal_
 
 
 @click.group()
@@ -16,26 +15,37 @@ def cli():
 @cli.command()
 @click.argument("file", type=click.File("r+"))
 @click.option(
-    "-d", "--date", type=click.DateTime(formats=["%Y-%m-%d"]),
-    default=dt.date.today().strftime(defaults.DATE_FMT), callback=lambda ctx, param, value: value.date(),
-    help="Date to use (defaults to today)"
+    "-d",
+    "--date",
+    type=click.DateTime(formats=["%Y-%m-%d"]),
+    default=dt.date.today().strftime(defaults.DATE_FMT),
+    callback=lambda ctx, param, value: value.date(),
+    help="Date to use (defaults to today)",
 )
 @click.option("-i", "--interactive", is_flag=True, help="Interactive mode")
-@click.option("-t", "--write-top", is_flag=True, help="Write output to the top of the journal file")
-@click.option("-b", "--write-bottom", is_flag=True, help="Write output to the bottom of the journal file")
+@click.option(
+    "-t",
+    "--write-top",
+    is_flag=True,
+    help="Write output to the top of the journal file",
+)
+@click.option(
+    "-b",
+    "--write-bottom",
+    is_flag=True,
+    help="Write output to the bottom of the journal file",
+)
 def fill(file, date, interactive, write_top, write_bottom):
     """
     Fill habits on a given date using FILE.
     """
-    records = journal.fill_day(
+    records = journal_.fill_day(
         file.name,
         date,
         interactive,
     )
     if records:
-        records_str = (
-            "\n".join([str(record) for record in records]) if records else ""
-        )
+        records_str = "\n".join([str(record) for record in records]) if records else ""
         if write_top:
             content = file.read()
             file.seek(0)
@@ -51,23 +61,57 @@ def fill(file, date, interactive, write_top, write_bottom):
 @cli.command()
 @click.argument("file", type=click.File("r"))
 @click.option(
-    "-s", "--start", type=click.DateTime(formats=["%Y-%m-%d"]),
+    "-s",
+    "--start",
+    type=click.DateTime(formats=["%Y-%m-%d"]),
     callback=lambda ctx, param, value: value.date() if value else None,
-    help="Start date"
+    help="Start date",
 )
 @click.option(
-    "-e", "--end", type=click.DateTime(formats=["%Y-%m-%d"]),
-    default=dt.date.today().strftime(defaults.DATE_FMT), callback=lambda ctx, param, value: value.date(),
-    help="End date"
+    "-e",
+    "--end",
+    type=click.DateTime(formats=["%Y-%m-%d"]),
+    default=dt.date.today().strftime(defaults.DATE_FMT),
+    callback=lambda ctx, param, value: value.date(),
+    help="End date",
 )
 @click.option("-n", "--name", help="Filter by habit name")
 def filter(file, start, end, name):
     """
     Filter habit records using FILE.
     """
-    records = journal.filter(file.name, start, end, name)
+    records = journal_.filter(file.name, start, end, name)
     if records:
         records_str = "\n".join([str(record) for record in records])
         click.echo(records_str)
     else:
         click.echo(f"{defaults.COMMENT_CHAR} No records found")
+
+
+@cli.group()
+def config():
+    pass
+
+
+@config.group()
+def set():
+    pass
+
+
+@set.command()
+@click.argument("path", type=click.Path())
+def journal(path):
+    """
+    Set the journal file path.
+    """
+    config_.set("journal", path, "CLI")
+    click.echo(f"Journal file path set to {path}")
+
+
+@config.command()
+def get():
+    """
+    Get all configuration settings.
+    """
+    with open(defaults.APPDATA_PATH + "/config.ini") as f:
+        click.echo(f.read())
